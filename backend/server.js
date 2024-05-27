44066e67e0942cb10c83d8515621b0623af10144
arshnoor <arshnoor029@gmail.com>
2024-05-28 00:26:34 +0530

    backend second

---
+++ backend/server.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const app = express();
+
+// packages
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+// connection to DB and cloudinary
+const { connectDB } = require('./config/database');
+const { cloudinaryConnect } = require('./config/cloudinary');
+
+// routes
+const userRoutes = require('./routes/user');
+const profileRoutes = require('./routes/profile');
+const paymentRoutes = require('./routes/payments');
+const courseRoutes = require('./routes/course');
+
+
+// middleware 
+app.use(express.json()); // to parse json body
+app.use(cookieParser());
+app.use(
+    cors({
+        // origin: 'http://localhost:5173', // frontend link
+        origin: "*",
+        credentials: true
+    })
+);
+
+// Configure CORS options if needed (optional)
+// const corsOptions = {
+//     origin: 'http://localhost:5173', // Specify the allowed origin(s)
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed methods
+//     allowedHeaders: 'Content-Type,Authorization', // Specify the allowed headers
+//     credentials: true, // Enable Access-Control-Allow-Credentials
+//     optionsSuccessStatus: 204 // Some legacy browsers choke on 204
+//   };
+  
+//   // Use CORS middleware with options
+//   app.use(cors(corsOptions));
+
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: '/tmp'
+    })
+)
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server Started on PORT ${PORT}`);
+});
+
+// connections
+connectDB();
+cloudinaryConnect();
+
+// mount route
+app.use('/api/v1/auth', userRoutes);
+app.use('/api/v1/profile', profileRoutes);
+app.use('/api/v1/payment', paymentRoutes);
+app.use('/api/v1/course', courseRoutes);
+
+
+
+
+// Default Route
+app.get('/', (req, res) => {
+    // console.log('Your server is up and running..!');
+    res.send(`<div>
+    This is Default Route  
+    <p>Everything is OK</p>
+    </div>`);
+})
